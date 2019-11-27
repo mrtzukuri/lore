cbf9537981bf633d4d34e51009c8f0a257c02bc2
Gin Atkins <vcatkins@gmail.com>
2019-11-27 18:00:40 +1100

    Added all controller and views that don't depend on the leanring opportunities

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
+  get 'users/show'
+  get 'users/edit'
+  get 'users/update'
+  get 'users/destroy'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get 'frontend', to: 'pages#frontend'
+get 'profile', to: 'users#show'
+
+
 end
---
+++ test/models/skill_groups_profession_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class SkillGroupsProfessionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

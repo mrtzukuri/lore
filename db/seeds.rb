9202498e5ccf0a40f423a397e2837791b8c852dd
joseph lam <jvlam318@gmail.com>
2019-11-27 11:25:25 +1100

    Add dotenv - Protect my secret data in .env file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "json"
+require "rest-client"
+
+puts 'Creating learning opportunities from eventbrite...'
+response = RestClient.get "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+eventbrite_learning_opportunities = JSON.parse(response)
+
+restaurants_attributes = []
+
+eventbrite_learning_opportunities["drinks"].each do |element|
+  LearningOpportunity.create!(
+    name: element["strIngredient1"]
+    )
+end
+
+puts 'Finished!'

47a54f32e83b7aed42aa3b4e576f4ff5c9a0c5f1
Allen Liao <a@tzukuri.com>
2019-11-27 14:30:40 +1100

    Created seed file with skills, professions, and skill groups including relationships between them.

--- db/migrate/20191126030754_create_join_table_skill_groups_skills.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableSkillGroupsSkills < ActiveRecord::Migration[5.2]
-  def change
-    create_join_table :skills, :skill_groups do |t|
-      # t.index [:skill_id, :skill_group_id]
-      # t.index [:skill_group_id, :skill_id]
-    end
-  end
-end

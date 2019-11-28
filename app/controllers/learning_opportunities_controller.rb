5ffa609cd66a4e1e278aec231333edb61a4f2371
joseph lam <jvlam318@gmail.com>
2019-11-28 13:24:25 +1100

    add links for home - index - show - add LO fixed naming convention for controller

---
+++ app/controllers/learning_opportunities_controller.rb
@@ -0,0 +1,52 @@
+class LearningOpportunitiesController < ApplicationController
+  before_action :set_params, only: %i[create update]
+  before_action :set_learningopportunity, only: %i[show edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index show]
+
+  def index
+    @learningopportunities = LearningOpportunity.all
+  end
+
+  def show
+  end
+
+  def new
+    @learningopportunity = LearningOpportunity.new
+  end
+
+  def create
+    @learningopportunity = LearningOpportunity.new(set_params)
+    if @learningopportunity.save
+      redirect_to learningopportunity_path(@learningopportunity)
+    else
+      render :new
+    end
+  end
+
+  def edit; end
+
+  def update
+    @learningopportunity.update(set_params)
+    if @learningopportunity.save
+      redirect_to learningopportunity_path(@learningopportunity)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @learningopportunity.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_params
+    params.require(:learningopportunity).permit!
+  end
+
+  def set_learningopportunity
+    @learningopportunity = LearningOpportunity.find(params[:id])
+  end
+
+end
---
+++ test/controllers/learningopportunities_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LearningopportunitiesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end

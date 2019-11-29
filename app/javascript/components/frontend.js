632e456623fd00adfefc8bf416049a279506b18f
Gin Atkins <vcatkins@gmail.com>
2019-11-29 11:35:02 +1100

    Added indication of which level selected, and if statement to ensure junior skills exit before event listeners run.

--- app/javascript/components/frontend.js
+++ app/javascript/components/frontend.js
@@ -7,24 +7,33 @@ const toggleLevelSkills = function() {
   const mid_skills = document.querySelectorAll('.Mid-Level');
   const senior_skills = document.querySelectorAll('.Senior');
 
+  if(junior){
+    junior.addEventListener('click', (event) => {
+      mid.classList.remove("selected");
+      junior.classList.add("selected");
+      senior.classList.remove("selected");
+      junior_skills.forEach(row => row.classList.remove("hidden"));
+      mid_skills.forEach(row => row.classList.add("hidden"));
+      senior_skills.forEach(row => row.classList.add("hidden"));
 
-  junior.addEventListener('click', (event) => {
-    junior_skills.forEach(row => row.classList.remove("hidden"));
-    mid_skills.forEach(row => row.classList.add("hidden"));
-    senior_skills.forEach(row => row.classList.add("hidden"));
+    })
 
-  })
+    mid.addEventListener('click', (event) => {
+      mid.classList.add("selected");
+      junior.classList.remove("selected");
+      senior.classList.remove("selected");
+      mid_skills.forEach(row => row.classList.remove("hidden"));
+      senior_skills.forEach(row => row.classList.add("hidden"));
 
-  mid.addEventListener('click', (event) => {
-    mid_skills.forEach(row => row.classList.remove("hidden"));
-    senior_skills.forEach(row => row.classList.add("hidden"));
+    })
 
-  })
-
-  senior.addEventListener('click', (event) => {
-    mid_skills.forEach(row => row.classList.remove("hidden"));
-    senior_skills.forEach(row => row.classList.remove("hidden"));
-  })
-
+    senior.addEventListener('click', (event) => {
+      mid.classList.remove("selected");
+      junior.classList.remove("selected");
+      senior.classList.add("selected");
+      mid_skills.forEach(row => row.classList.remove("hidden"));
+      senior_skills.forEach(row => row.classList.remove("hidden"));
+    })
+  }
 }
 export { toggleLevelSkills }

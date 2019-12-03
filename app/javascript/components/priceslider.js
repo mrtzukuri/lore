b147a47db0100fc1fc2be86937bd364e646e0ac8
Allen Liao <a@tzukuri.com>
2019-12-03 16:51:34 +1100

    reverted

--- app/javascript/components/priceslider.js
+++ app/javascript/components/priceslider.js
@@ -3,12 +3,10 @@ const priceSlider = function() {
   var rangeBullet = document.getElementById("rs-bullet");
 
   rangeSlider.addEventListener("input", showSliderValue, false);
-  if(rangeSlider){
-    function showSliderValue() {
-      rangeBullet.innerHTML = rangeSlider.value;
-      var bulletPosition = (rangeSlider.value / rangeSlider.max);
-      rangeBullet.style.left = (bulletPosition * 200) + "px";
-    }
+  function showSliderValue() {
+    rangeBullet.innerHTML = rangeSlider.value;
+    var bulletPosition = (rangeSlider.value / rangeSlider.max);
+    rangeBullet.style.left = (bulletPosition * 200) + "px";
   }
 }
 
